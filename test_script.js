4742c738b1e509447dbba0231eb598862beabc69
mel <mchuam@gmail.com>
2018-05-01 03:37:36 +0000

    added listRows function, next up is getting moment so that we can use dates properly

---
+++ test_script.js
@@ -0,0 +1,44 @@
+const { Client } = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new Client({
+  user      : settings.user,
+  password  : settings.password,
+  database  : settings.database,
+  host      : settings.hostname,
+  port      : settings.port,
+  ssl       : settings.ssl
+});
+
+const query = {
+  text: 'SELECT first_name, last_name, birthdate FROM famous_people where first_name = $1::text',
+  // rowMode: 'array',
+  values: ['Paul']
+};
+
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(query, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+
+  listRows(result);
+  client.end();
+
+  // console.log(results);
+
+  });
+});
+
+function listRows(list) {
+   list.rows.forEach( (row, index) => {
+    let date = new Date(row.birthdate);
+    console.log(date.getMonth());
+// add moment.js to get dates in here
+    console.log(`${index}: ${row.first_name} ${row.last_name}, born ${row.birthdate}`);
+  });
+}
